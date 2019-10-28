21ccd358b35de1fcfdb8efbb6b08fa1c096500ac
Marian Q <mariansofia1999@hotmail.com>
2019-10-28 09:06:03 -0500

    Mongo conectado
    Conexión mongo db Node.js

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
+var exphbs = require('express-handlebars');
 const express = require("express");
 var bodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const createRoutes = require('./routes.js');
+
+// importar file system
+var fs = require('fs');
 const app = express();
 
 const port = 5000;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", (request, response) =>{
-    response.sendFile(`${__dirname}/public/index.html`);
+const url = 'mongodb://localhost:27017';
+const dbName = 'Store';
+const client = new MongoClient(url);
+
+// conectar el cliente de mongo
+client.connect(function(err) {
+    // asegurarnos de que no existe un error
+    assert.equal(null, err);
+
+    console.log('connected');
+
+    // conectamos el cliente a la base de datos que necesitamos
+    const db = client.db(dbName);
+
+  
+    createRoutes(app, db);
+
+    app.listen(port, ()=>{
+        console.log(`Servidor iniciado en el puerto ${port}`);
+    });
+
 });
 
-app.get("/tienda", (request, response) =>{
-    response.sendFile(`${__dirname}/public/store.html`);
-})
 
-app.listen(port, ()=>{
-    console.log(`Servidor iniciado en el puerto ${port}`);
-});
+
---
+++ routes.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+
+function createRoutes( app, db){
+    app.get("/", (request, response) =>{
+        response.sendFile(`${__dirname}/public/index.html`);
+    });
+    
+    app.get("/tienda", (request, response) =>{
+        response.sendFile(`${__dirname}/public/store.html`);
+    });
+    
+    app.get("/comprar", (request, response) =>{
+        response.sendFile(`${__dirname}/public/checkout.html`);
+    });
+}
+
+module.exports = createRoutes;
